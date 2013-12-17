daecd303e65ca8c3ebf81999215cdbf97b1fe87a
Sam Sartor <eeky.sam@gmail.com>
2013-12-17 08:45:55 -0700

    Config Enable Run
    Fixed workspace
    Added plugin.yml
    Enable method

--- Curioseract/src/zapcloud/curioseract/Curioseract.java
+++ Curioseract/src/zapcloud/curioseract/Curioseract.java
@@ -4,5 +4,15 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Curioseract extends JavaPlugin
 {
+	@Override
+	public void onEnable()
+	{
 
+	}
+
+	@Override
+	public void onDisable()
+	{
+
+	}
 }
